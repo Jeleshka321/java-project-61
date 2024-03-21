2a8a04a1468818850fab20fc3fefce375d6c6081
Jeleshka321 <78028927+Jeleshka321@users.noreply.github.com>
2024-03-21 07:24:19 +0700

    Added gradle

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+application {
+    mainClass = "hexlet.code.App"
+}
