c0ed8e10e7e71bc791ce73714bc01fdb954871b9
Jeleshka321 <78028927+Jeleshka321@users.noreply.github.com>
2024-03-28 17:03:00 +0700

    Fixed gitigno

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,54 @@
 package hexlet.code;
 
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.startGame(args);
-    }
+import java.util.Scanner;
 
-}
+public class App {
+        public static void startGame(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            final int three = 3;
+            final int fourth = 4;
+            final int five = 5;
+            final int six = 6;
+
+            System.out.print("Please enter the game number and press Enter ");
+            System.out.print("6 - Prime ");
+            System.out.print("5 - Progression ");
+            System.out.print("4 - GCD ");
+            System.out.print("3 - Calculator ");
+            System.out.print("2 - Even ");
+            System.out.print("1 - Greet ");
+            System.out.print("0 - Exit ");
+            int gameNumber = scanner.nextInt();
+            System.out.println("Your choice: " + gameNumber);
+
+
+            switch (gameNumber) {
+                case 0:
+                    System.out.println("Exiting the program. Goodbye!");
+                    break;
+                case 1:
+                    Cli.getName(args);
+                    break;
+                case 2:
+                    Even.evenGame(args);
+                    break;
+                case three:
+                    Calc.calcGame(args);
+                    break;
+                case fourth:
+                    GCD.gcdGame(args);
+                    break;
+                case five:
+                    Prog.progGame(args);
+                    break;
+                case six:
+                    Prime.primeGame(args);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid game number.");
+
+                    scanner.close();
+            }
+        }
+    }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,51 +2,12 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-        public static void startGame(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        final int three = 3;
-        final int fourth = 4;
-        final int five = 5;
-        final int six = 6;
-
-        System.out.print("Please enter the game number and press Enter ");
-        System.out.print("6 - Prime ");
-        System.out.print("5 - Progression ");
-        System.out.print("4 - GCD ");
-        System.out.print("3 - Calculator ");
-        System.out.print("2 - Even ");
-        System.out.print("1 - Greet ");
-        System.out.print("0 - Exit ");
-        int gameNumber = scanner.nextInt();
-        System.out.println("Your choice: " + gameNumber);
-
-
-        switch (gameNumber) {
-                case 0:
-                        System.out.println("Exiting the program. Goodbye!");
-                        break;
-                case 1:
-                        Names.getName(args);
-                        break;
-                case 2:
-                        Even.evenGame(args);
-                        break;
-                case three:
-                        Calc.calcGame(args);
-                        break;
-                case fourth:
-                        GCD.gcdGame(args);
-                        break;
-                case five:
-                        Prog.progGame(args);
-                        break;
-                case six:
-                        Prime.primeGame(args);
-                        break;
-                default:
-                        System.out.println("Invalid choice. Please enter a valid game number.");
-
-        scanner.close();
-    }
-}
+        public static void getName(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Welcome to the Brain Games!");
+                System.out.print("May I have your name? ");
+                String name = scanner.next();
+                System.out.println("Hello, " + name + "!");
+                scanner.close();
+        }
 }
