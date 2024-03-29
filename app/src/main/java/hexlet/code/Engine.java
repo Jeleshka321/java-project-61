8f4ada2ec33f80794a5adf88c93c8e327bbc51ec
Jeleshka321 <78028927+Jeleshka321@users.noreply.github.com>
2024-03-29 09:37:08 +0700

    Fixed App and added Engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,6 +1,13 @@
-package hexlet.code;
-import java.util.Random;
-import java.util.Scanner;
+//package hexlet.code;
+//import java.util.Random;
+//import java.util.Scanner;
 
-public class Engine {
-}
+//public class Engine { int correctAnswers = 0;
+//    final int corScore = 3;
+        //Scanner scanner = new Scanner(System.in);
+                //Random random = new Random();
+                //String[] operators = {"+", "-", "*"};
+                //int correctAnswers = 0;
+//final int maxNumber = 100;
+//final int questions = 3;
+
